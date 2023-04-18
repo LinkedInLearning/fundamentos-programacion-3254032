96ffca8e7fa35721eee77b49165f6a703235f0fd
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2023-04-18 21:22:44 +0200

    fix example

--- Unidad_04/04_02/final/app.py
+++ Unidad_04/04_02/final/app.py
@@ -4,10 +4,10 @@ nota = 'Ganadora Premio Novel de la Paz'
 
 primer_nombre_cap = primer_nombre.capitalize()
 segundo_nombre_cap = segundo_nombre.capitalize()
-award_ubicacion = nota.find('award: ')
-award_texto = nota[7:]
+premio_ubicacion = nota.find('Premio')
+premio_texto = nota[7:]
 
 print(primer_nombre_cap)
 print(segundo_nombre_cap)
-print(award_ubicacion)
-print(award_texto)
+print(premio_ubicacion)
+print(premio_texto)
